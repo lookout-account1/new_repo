f89c42e07be80ab77be1366e12b09cccdd85c96a
lookout-account1 <91314813+lookout-account1@users.noreply.github.com>
2023-09-27 14:28:34 +0530

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import path from 'path'
 import ts from 'rollup-plugin-typescript2'
 import replace from '@rollup/plugin-replace'
 import json from '@rollup/plugin-json'
-// test
+// testing
 if (!process.env.TARGET) {
   throw new Error('TARGET package must be specified via --environment flag.')
 }
